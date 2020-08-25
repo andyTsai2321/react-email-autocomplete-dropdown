de752610bb7ea96e61c71d0f63699b80ccc213c8
andyTsai2321 <s106667@gmail.com>
2020-08-25 10:53:27 +0800

    update file

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base.js'); // 引用公共配置
+const baseConfig = require('./webpack.base.js');
 
 const devConfig = {
-  mode: 'development', // 开发模式
-  entry: path.join(__dirname, "../example/src/index.js"), // 项目入口，处理资源文件的依赖关系
+  mode: 'development',
+  entry: path.join(__dirname, "../example/src/index.js"), 
   output: {
     path: path.join(__dirname, "../example/src/"),
-    filename: "bundle.js", // 使用webpack-dev-sevrer启动开发服务时，并不会实际在`src`目录下生成bundle.js，打包好的文件是在内存中的，但并不影响我们使用。
+    filename: "bundle.js", 
   },
   module: {
     rules: [
@@ -25,9 +25,9 @@ const devConfig = {
   devServer: {
     contentBase: path.join(__dirname, '../example/src/'),
     compress: true,
-    host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
-    port: 3001, // 启动端口为 3001 的服务
-    open: true // 自动打开浏览器
+    host: '127.0.0.1', 
+    port: 3001, 
+    open: true 
   },
 };
-module.exports = merge(devConfig, baseConfig); // 将baseConfig和devConfig合并为一个配置
+module.exports = merge(devConfig, baseConfig); 
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base.js'); // 引用公共的配置
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 用于将组件的css打包成单独的文件输出到`lib`目录中
+const baseConfig = require('./webpack.base.js'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const prodConfig = {
-  mode: 'production', // 开发模式
+  mode: 'production', 
   entry: path.join(__dirname, "../src/index.js"),
   output: {
     path: path.join(__dirname, "../lib/"),
     filename: "index.js",
-    libraryTarget: 'umd', // 采用通用模块定义
-    libraryExport: 'default', // 兼容 ES6 的模块系统、CommonJS 和 AMD 模块规范
+    libraryTarget: 'umd', 
+    libraryExport: 'default',
   },
   module: {
     rules: [
@@ -22,10 +22,10 @@ const prodConfig = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "main.min.css" // 提取后的css的文件名
+      filename: "main.min.css"
     })
   ],
-  externals: { // 定义外部依赖，避免把react和react-dom打包进去
+  externals: {
     react: {
       root: "React",
       commonjs2: "react",
@@ -41,4 +41,4 @@ const prodConfig = {
   },
 };
 
-module.exports = merge(prodConfig, baseConfig); // 将baseConfig和prodConfig合并为一个配置
+module.exports = merge(prodConfig, baseConfig);
